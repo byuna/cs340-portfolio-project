970c25b43a403f79cbb75821da5febbce7580f59
Abraham Byun <byuna@flip1.engr.oregonstate.edu>
2022-11-02 22:01:51 -0700

    Added formatting for tables in CSS

--- public/js/add_customer.js
+++ public/js/add_customer.js
@@ -18,4 +18,77 @@ addCustomerForm.addEventListener("submit", function (e) {
   let customerLastNameValue = inputCustomerLastName.value;
   let customerPhoneValue = inputCustomerPhone.value;
   let customerEmailValue = inputCustomerEmail.value;
+
+  // Create a JSON object with our data.
+  let data = {
+    customer_first_name: customerFirstNameValue,
+    customer_last_name: customerLastNameValue,
+    customer_phone: customerPhoneValue,
+    customer_email: customerEmailValue
+  };
+
+  // Setup the AJAX request.
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "/add-customer", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+
+  // Tell AJAX request how to resolve.
+  xhttp.onreadystatechange = () => {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+      // Add the new datat to the table
+      addRowToTable(xhttp.response);
+
+      // Clear the input fields for another transaction.
+      inputCustomerFirstName.value = '';
+      inputCustomerLastName.value = '';
+      inputCustomerPhone.value = '';
+      inputCustomerEmail.value = '';
+
+    } else if (xhttp.readyState == 4 && xhttp.status != 200) {
+      console.log("There was an error with the input.");
+    }
+  }
+
+  // Send the request and wait for the response.
+  xhttp.send(JSON.stringify(data));
 })
+
+// Creats a single row from an Object representing a single record from "Customers" table.
+addRowToTable = (data) => {
+  
+  // Get a reference to the current table on the page and clear it out.
+  let currentTable = document.getElementById('customers-table');
+
+  // The get the location where we should insert the new row (end of table).
+  let newRowIndex = currentTable.rows.length;
+
+  // Get a reference to the new row from the database query (last object).
+  let parsedData = JSON.parse(data);
+  let newRow = parsedData[parsedData.lengh - 1];
+
+  // Create a row and 5 cells.
+  let row = document.createElement("TR");
+  let customerIdCell = document.createElement("TD");
+  let customerFirstNameCell = document.createElement("TD");
+  let customerLastNameCell = document.createElement("TD");
+  let customerPhoneCell = document.createElement("TD");
+  let customerEmailCell = document.createElement("TD");
+  
+  // Fill the cells with the correct data.
+  customerIdCell.innerText = newRow.cusotmer_id;
+  customerFirstNameCell.innerText = newRow.customer_first_name;
+  customerLastNameCell.innerText = newRow.customer_last_name;
+  customerPhoneCell.innerText = newRow.customer_phone;
+  customerEmailCell.innerText = newRow.customer_email;
+
+  // Add the cells to the row.
+  row.appendChild(customerIdCell);
+  row.appendChild(customerFirstNameCell);
+  row.appendChild(customerLastNameCell);
+  row.appendChild(customer.customerPhoneCell);
+  row.appendChild(customerEmailCell);
+
+  currentTable.appendChild(row);
+}
+
